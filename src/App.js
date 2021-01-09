d621cd7927678fcaf48d3457852019f503238efa
gusr27 <gus.richardson>
2021-01-09 17:18:41 -0500

    Responsive app bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button,Grid,AppBar, Toolbar, Typography, InputBase} from '@material-ui/core/';
+import {Button,Grid,AppBar, Toolbar, Typography, InputBase,GridList} from '@material-ui/core/';
 
 function App() {
   return (
@@ -10,12 +10,13 @@ function App() {
       Get showcase container ready.
     */
     <Grid container>
-      <Grid item xs={3} style={{backgroundColor:"#023e8a"}}>
-        <Typography variant="h6" style={{color:"#caf0f8"}}>
+    <Grid container  style={{backgroundColor:"#312c51"}}>
+      <Grid item xs={12} sm={3} >
+        <Typography variant="h6" style={{color:"#f0c38e"}}>
           Dubbed Anime Search Engine
         </Typography>
       </Grid>
-      <Grid item xs={6}>  <AppBar style={{backgroundColor:"#0077b6"}} position="static">
+      <Grid item xs={12} sm={6}>  <AppBar style={{backgroundColor:"#48426d"}} position="static">
           <Toolbar>
           <InputBase
           placeholder="Search..."
@@ -23,9 +24,20 @@ function App() {
 
           </Toolbar>
         </AppBar></Grid>
-      <Grid item xs={3} style={{backgroundColor:"#023e8a"}}></Grid>
+      <Grid item xs={0} sm={6}></Grid>
     </Grid>
-
+      <Grid container>
+        <Grid item x={3}></Grid>
+        <Grid item x={6} justifyContent="center" style={{backgroundColor:"gray", width:"50%"}}>
+          <GridList cellheight={160} cols={2}>
+            <Typography>
+              Show Here!
+            </Typography>
+          </GridList>
+        </Grid>
+        <Grid item x={3}></Grid>
+      </Grid>
+    </Grid>
 
 
   );
