03fb6830f16d25ab930e5e5fcd0d46d731ba53c0
gusr27 <gus.richardson>
2021-01-14 12:04:44 -0500

    Added search and showcase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,109 @@
+import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import {Button,Grid,AppBar, Toolbar, Typography, InputBase,GridList, Card, CardContent} from '@material-ui/core/';
+import shows from './shows.js'
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      title:'',
+      results:[],
+      showcase: true
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.changeTitle = this.changeTitle.bind(this);
+    this.searchDb = this.searchDb.bind(this);
+    this.renderShowcase = this.renderShowcase.bind(this)
+  }
+  handleChange(e){
+    this.setState({
+      title:e.target.value
+    })
+    if(e.target.value.length <2){
+      this.setState({
+        showcase:true
+      })
+    }
+    else{
+      this.setState({
+        showcase:false
+      })
+    }
+  }
+  changeTitle(){
+    return (this.state.title)
+  }
+  searchDb = () => {
 
-function App() {
+    if(this.state.title.length > 1){
+
+       let items = shows.filter(show => show.title.toLowerCase().includes(this.state.title)).map((result, i) =>
+
+            <Card key={i}>
+              <Typography>
+              {result.title}
+              </Typography>
+            </Card>
+
+      )
+
+      return items
+    }
+
+
+  }
+  renderShowcase = () =>{
+
+      return(
+          <Grid item xs={6} justify="center" alignItems="center" >
+         <GridList id="showcase2"cellheight={160} cols={1}>
+
+          <Card>
+            <CardContent>
+              <Typography>
+                Attack on Titan
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <Typography>
+                Attack on Titan
+              </Typography>
+            </CardContent>
+          </Card>
+        </GridList>
+        <GridList id="showcase"cellheight={160} cols={2}>
+
+         <Card>
+           <CardContent>
+             <Typography>
+               Attack on Titan
+             </Typography>
+           </CardContent>
+         </Card>
+         <Card>
+           <CardContent>
+             <Typography>
+               Attack on Titan
+             </Typography>
+           </CardContent>
+         </Card>
+       </GridList>
+        </Grid>
+      )
+
+
+  }
+
+  render(){
+
   return (
     /*
       Get App to full colo
     */
+
     <Grid container style={{backgroundColor:"#393e46"}}>
     <Grid container  style={{backgroundColor:"#222831"}}>
       <Grid item xs={12} sm={3} >
@@ -18,6 +115,8 @@ function App() {
           <Toolbar>
           <InputBase
           placeholder="Search..."
+          onChange={this.handleChange}
+          value={this.state.title}
           />
 
           </Toolbar>
@@ -27,51 +126,13 @@ function App() {
       <Grid id="default-display" container justify="center" alignItems="center" style={{paddingTop:"100px"}} direction="column">
 
         <Grid item xs={6} justify="center" alignItems="center" >
+          {this.state.showcase ? this.renderShowcase(): null}
           <GridList id="showcase"cellheight={160} cols={2}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
-          </GridList>
+
+           {this.searchDb()}
+         </GridList>
           <GridList id="showcase2"cellheight={160} cols={1}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>
-                  Attack on Titan
-                </Typography>
-              </CardContent>
-            </Card>
+              {this.searchDb()}
           </GridList>
         </Grid>
 
@@ -80,6 +141,7 @@ function App() {
 
 
   );
+  }
 }
 
 export default App;
---
+++ src/shows.js
@@ -0,0 +1,18 @@
+const shows = [
+  {
+    title:"Attack on Titan",
+    service:["funi"]
+  },
+  {
+    title:"Konosuba",
+    service:["vrv"]
+  },
+  {
+    title:"Black Clover",
+    service:"funi"
+  },
+  {
+    title:"Attack of the Show"
+  }
+];
+export default shows
